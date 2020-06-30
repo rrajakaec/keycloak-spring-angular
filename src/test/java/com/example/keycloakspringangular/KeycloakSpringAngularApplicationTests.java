a7c82e52cf7f7f3caf1597b5957bc56385a80526
Rintu Rajak <rintu.rajak@datazi.com>
2020-06-30 21:21:21 +0530

    initial commit

---
+++ src/test/java/com/example/keycloakspringangular/KeycloakSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
